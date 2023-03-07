1caf4fa8d0708524ee0b7cfb7080c5f1572dcbee
SaverioCascelli <saverio.cascelli@gmail.com>
2023-03-07 14:21:14 +0100

    delete original main

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -3,6 +3,6 @@ package org.lessons.java;
 public class StampaSaluto {
     public static void main(String[] args) {
         System.out.println("Ciao!");
-        System.out.println("secondo saluto");
+
     }
 }
